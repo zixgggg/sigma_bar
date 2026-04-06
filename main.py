194d084da208c05e238eb32cd819c8a40e9166ee
zixgggg <zixzix035@gmail.com>
2026-04-06 16:49:57 +0800

    try to use configparser manage cmd and config(not finish

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import dearpygui.dearpygui as dpg
 import subprocess
 import time
+import configparser
 dpg.create_context()
-d=subprocess.check_output(["date '+%Y-%m-%d %H:%M:%S'"],shell=True).decode("utf-8").strip()
+config = configparser.ConfigParser()
+config.read("config.ini")
+cmd=config["time"]["commend"]
+d=subprocess.check_output(cmd,shell=True).decode("utf-8").strip()
+#d=subprocess.check_output(["date '+%Y-%m-%d %H:%M:%S'"],shell=True).decode("utf-8").strip()
 c=subprocess.check_output(["""sar -u 1 1 | tail -1 | awk '{printf "%.2f%%", 100 - $NF}'"""],shell=True).decode("utf-8").strip()
 
 with dpg.window(tag="pw"):
@@ -12,7 +17,7 @@ with dpg.window(tag="pw"):
         dpg.add_text(c,tag="cpu_tag")
 
 dpg.set_primary_window("pw", True)
-dpg.create_viewport(title="sigma_bar",resizable=False,x_pos=0,y_pos=1000,width=1920,height=80,decorated=False,always_on_top=True,disable_close=True)
+dpg.create_viewport(title="sigma_bar",resizable=False,x_pos=0,y_pos=0,width=1920,height=30,decorated=False,always_on_top=True,disable_close=True)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 while dpg.is_dearpygui_running():
