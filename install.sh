97b6448c24572a80529ccadb8ad6d8350e4ea790
zixgggg <zixzix035@gmail.com>
2026-06-19 16:14:36 +0800

    check ~/.local/bin exist,avoid cp turn ~/.local/bin to executable file

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
-#curl https://raw.githubusercontent.com/zixgggg/sigmabar/refs/heads/main/install.sh
 git clone https://github.com/zixgggg/sigmabar.git
 cd sigmabar/
-cp sigmabar $HOME/.local/bin
-mkdir $HOME/.config/sigmabar/
+chmod +x sigmabar
+mkdir -p $HOME/.local/bin
+cp sigmabar $HOME/.local/bin/sigmabar
+mkdir -p $HOME/.config/sigmabar/
 cp config.ini $HOME/.config/sigmabar/
 
