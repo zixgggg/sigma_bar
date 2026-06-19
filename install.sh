8fe61996675b38126a5dc70234e5e801854a4147
zixgggg <zixzix035@gmail.com>
2026-06-19 17:02:11 +0800

    edit export path part

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp sigmabar $HOME/.local/bin/sigmabar
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
     # 檢查 ~/.bashrc 裡是不是早就已經有這行字了
     if ! grep -q '.local/bin' "$HOME/.bashrc" 2>/dev/null; then
-		echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+		echo export PATH="$HOME/.local/bin:$PATH" >> "$HOME/.bashrc"
 	fi
 fi
 mkdir -p $HOME/.config/sigmabar/
